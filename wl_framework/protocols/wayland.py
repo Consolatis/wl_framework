c6cbe0226c25fee7182f3063201b44abebaa9ebe
Consolatis <35009135+Consolatis@users.noreply.github.com>
2023-02-17 13:22:09 +0100

    protocols/wayland: bind to outputs automatically

--- wl_framework/protocols/wayland.py
+++ wl_framework/protocols/wayland.py
@@ -4,6 +4,7 @@ import struct
 from collections import defaultdict
 
 from .base import (
+	ArgInt32,
 	ArgUint32,
 	ArgString,
 	Interface,
@@ -43,6 +44,7 @@ class Display(Interface):
 		self.add_event(self.on_delete_id)
 		connection.add_event_handler(self)
 		self.registry = self.get_registry()
+		self.outputs = list()
 
 	# Wayland events
 	def on_error(self, data, fds):
@@ -72,6 +74,22 @@ class Display(Interface):
 	def on_initial_sync(self):
 		self.seat = Seat(self._connection)
 
+	def on_output_new(self, output):
+		self.outputs.append(output)
+
+	def on_output_del(self, output_global):
+		for output in tuple(self.outputs):
+			if output.global_id == output_id:
+				self.outputs.remove(output)
+				self.log(f"Output {output_id} removed, {len(self.outputs)} outputs remaining")
+				return
+		self.log(f"We can't remove output {output_id} because we don't know anything about it")
+
+	def get_output_by_id(self, output_id):
+		for output in self.outputs:
+			if output.obj_id == output_id:
+				return output
+		raise Exception(f"No output with id {output_id}")
 
 class Registry(Interface):
 	def __init__(self, connection, obj_id):
@@ -93,6 +111,13 @@ class Registry(Interface):
 		self._registry[global_id] = (name, version)
 		self._interfaces[name].append(global_id)
 
+		if name == 'wl_output':
+			#self.log("Creating new output:", global_id, "with server supported version", version)
+			output = Output(self._connection, global_id)
+			self.do_bind(output, ignore_sync=True)
+			self._connection.add_event_handler(output)
+			self._connection.display.on_output_new(output)
+
 	def on_global_remove(self, data, fds):
 		_, global_id = ArgUint32.parse(data)
 		if global_id not in self._registry:
@@ -102,6 +127,9 @@ class Registry(Interface):
 		self._interfaces[name].remove(global_id)
 		if len(self._interfaces[name]) == 0:
 			del self._interfaces[name]
+
+		if name == 'wl_output':
+			self._connection.display.on_output_del(global_id)
 
 		# TODO: Should we notify all current instances of this global?
 
@@ -159,3 +187,81 @@ class Seat(Interface):
 		if self.version < 5:
 			return
 		pass
+
+class Output(Interface):
+	MODE_CURRENT = 1
+	MODE_PREFERRED = 2
+	def __init__(self, connection, global_id):
+		super().__init__(connection)
+		self.set_name('wl_output')
+		self.set_version(4)
+		self.global_id = global_id
+		self._events = (
+			self.on_geometry,
+			self.on_mode,
+			self.on_done,
+			self.on_scale,
+			self.on_name,
+			self.on_description
+		)
+		self.width = 0
+		self.height = 0
+		self.name = None
+		self.description = None
+
+	# Wayland events
+	def on_geometry(self, data, fds):
+		offset = 0
+		consumed, x = ArgInt32.parse(data[offset:])
+		offset += consumed
+		consumed, y = ArgInt32.parse(data[offset:])
+		offset += consumed
+		consumed, w_phys = ArgInt32.parse(data[offset:])
+		offset += consumed
+		consumed, h_phys = ArgInt32.parse(data[offset:])
+		offset += consumed
+		consumed, sub = ArgInt32.parse(data[offset:])
+		offset += consumed
+		consumed, make = ArgString.parse(data[offset:])
+		offset += consumed
+		consumed, model = ArgString.parse(data[offset:])
+		offset += consumed
+		_, transform = ArgInt32.parse(data[offset:])
+
+		#self.log(f"on_geometry (version {self.version}): pos {x}|{y} phys {w_phys}x{h_phys} subpixel align {sub} {model} from {make} with transform {transform}")
+
+	def on_mode(self, data, fds):
+		_, _flags = ArgUint32.parse(data)
+		_, width = ArgInt32.parse(data[4:])
+		_, height = ArgInt32.parse(data[8:])
+		_, refresh = ArgInt32.parse(data[12:])
+		flags = list()
+		if _flags & Output.MODE_CURRENT:
+			self.width = width
+			self.height = height
+			flags.append('current')
+		if _flags & Output.MODE_PREFERRED:
+			flags.append('preferred')
+		flags = ', '.join(flags)
+		#self.log(f"on_mode (version {self.version}): {width}x{height}@{refresh} {flags}")
+
+	def on_done(self, data, fds):
+		#self.log("on_done")
+		pass
+
+	def on_scale(self, data, fds):
+		#self.log("on_scale")
+		pass
+
+	def on_name(self, data, fds):
+		_, name = ArgString.parse(data)
+		self.name = name
+		#self.log("Got name:", name)
+
+	def on_description(self, data, fds):
+		_, description = ArgString.parse(data)
+		self.description = description
+		#self.log("Got description:", description)
+
+	def __repr__(self):
+		return 'f{repr(super())}-{self.global_id}'
